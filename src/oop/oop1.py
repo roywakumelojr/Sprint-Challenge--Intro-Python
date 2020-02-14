9f2cafeb573cd8ccfa77269863aea2748cf22281
Roy Wakumelo <roywakumelojr@gmail.com>
2020-02-14 12:02:13 -0500

    oop files complete

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,42 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+### This is the primary base for the classes ###
+
+
+class Vehicle(object):
+    pass
+
+
+##### [Vehicle] #####
+# The base class for Ground Vehicle is Vehicle
+class GroundVehicle(Vehicle):
+    pass
+
+
+# The base class for Fright Vehicle is Vehicle
+class FlightVehicle(Vehicle):
+    pass
+
+
+##### [GroundVehicle] #####
+# The base class for the Car is GroundVehicle
+class Car(GroundVehicle):
+    pass
+
+
+# The base class for the Motorcycle is GroundVehicle
+class Motorcycle(GroundVehicle):
+    pass
+
+
+##### [FlightVehicle] #####
+# The base class for the Airplane is FrightVehicle
+class Airplane(FlightVehicle):
+    pass
+
+
+# The base class for the Starship is FrightVehicle
+class Starship(FlightVehicle):
+    pass
